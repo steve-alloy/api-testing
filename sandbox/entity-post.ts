b0e663110f2a6b925964138108fe9b5d65bc0d5c
steve-alloy <stevegiordano@Steves-MBP.lan1>
2022-01-20 20:07:30 -0500

    more_semantic_naming

--- sandbox/entity-post.ts
+++ sandbox/entity-post.ts
@@ -1,13 +1,10 @@
 import fs from "node:fs";
 import axios from "axios";
-
 import configObj from "../src/config";
 
 const ENDPOINT = "entities";
 const ENTITY_TOKEN = "P-MsTPWjyg80EF8WABaAEz";
-
 const url = configObj.baseUrl + ENDPOINT;
-
 const bodyObj = {
     "name": "driver_license",
     "extension": "jpg",
@@ -16,25 +13,25 @@ const bodyObj = {
 
 const postEntity = async (url: string, token: string): Promise<void> => {
     const entityUrl = `${url}/${token}/documents`;
-    const response = await axios.post(entityUrl, JSON.stringify(bodyObj), {
+    const responseFromEntity = await axios.post(entityUrl, JSON.stringify(bodyObj), {
         headers: {
             "Authorization": configObj.authorization,
             "Content-Type": "application/json"
         }
     });
 
-    const documentToken = response.data.document_token;
-    const data = fs.readFileSync("../images/license.jpg");
+    const documentToken = responseFromEntity.data.document_token;
+    const file = fs.readFileSync("../images/license.jpg");
     const entityUrlWithDocumentToken = `${url}/${token}/documents/${documentToken}`;
 
-    const formResponse = await axios.put(entityUrlWithDocumentToken, data, {
+    const responseFromEntityWithDocument = await axios.put(entityUrlWithDocumentToken, file, {
         headers: {
             "Content-Type": "application/octet-stream",
             "Authorization": configObj.authorization
         }
     });
     
-    console.log(formResponse.data);
+    console.log(responseFromEntityWithDocument.data);
 };
 
 postEntity(url, ENTITY_TOKEN);
